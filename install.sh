fbc225cf554af287eb0bfb6f285f472cdf2e8358
mikelikesto <66426421+mikelikesto@users.noreply.github.com>
2022-10-17 11:42:49 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
  #!/bin/bash
-chmod +x AutoDnsCloudflare.sh && sudo mv AutoDnsCloudflare.sh /bin 
+chmod +x AutoDnsCloudflare.sh && sudo mv AutoDnsCloudflare.sh /bin && gcc cldns.c -o cldns && sudo mv cldns /bin
